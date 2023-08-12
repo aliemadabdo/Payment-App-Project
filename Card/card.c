56c9f8ad0855a1cc4889a8ac095b3cb40b15cf2b
aliemadabdo <a.e.a3426@gmail.com>
2023-08-13 00:12:14 +0300

    ADD getCardExpiryDate Function

--- Card/card.c
+++ Card/card.c
@@ -27,9 +27,39 @@ EN_cardError_t getCardExpiryDate(ST_cardData_t *cardData){
 	/*
 	 This function will ask for card expiry date and store it into card data 
 	*/
+	int month = 0, year = 0, slash = 0 , nulll = 0;
 	
+	printf("Enter card expiry date MM/YY: ");
+	fflush(stdout);
+	scanf("%s",cardData->cardExpirationDate);
+	
+	#define exp cardData->cardExpirationDate  //just to simple writing the code
+	
+	if ( ( (exp[0] == '0') && (exp[1] >= '1') && (exp[1] <= '9') ) || ( (exp[0] == '1') && (exp[1] >= '0') && (exp[1] <= '2') ) )
+		month = 1;												/*check month 01 - 12*/
 
+	if (exp[2] == '/')											/*check front slash*/
+		slash = 1;
+		
+	if ((exp[3] == '2') && (exp[4] >= '3') && (exp[4] <= '9')) 	/*check years between 2023 to 2029*/ 
+		year = 1;
 	
+	if (exp[5] == '\0')											/*check for max string length = 5*/
+		nulll = 1;
 	
-	return CARD_OK
+	//printf("\nmonth: %d slash: %d year: %d nulll: %d \n",month,slash,year,nulll);
+	
+	if (month && year && slash && nulll)
+		return CARD_OK;
+	
+//  Other formating:
+//	if( ( ((exp[0] == '0') && (exp[1] >= 1) && (exp[1] <= 9)) || ( (exp[0] == '1') && (exp[1] >= 0) && (exp[1] <= 2)) )&& 
+//		 /*check if month 01 to 09*/                       /*check if month 10 to 12*/
+//		(  exp[2] == '/'					 /*check front slash*/									      )&&
+//		(  exp[3] == '2'					 /*years between 20(2)3 to 20(2)9*/ 				          )&&
+//		( (exp[4] >= '3') && (exp[4] <= '9') /*years between 202(3) to 202(9)*/ 						  )&&
+//		(  exp[5] == '\0') )				 
+//		return CARD_OK;
+	
+	return WRONG_EXP_DATE;
 }
--- main.c
+++ main.c
@@ -7,9 +7,12 @@ int main(void){
 	ST_cardData_t cardData;
 	
 	while( getCardHolderName(&cardData) == WRONG_NAME){
-		printf("Invalid name.\nPlease, enter a name between 20 to 24 characters \n");		
+		printf("\nInvalid name.\nPlease, enter a name between 20 to 24 characters \n");		
 	}
 	
+	while( getCardExpiryDate(&cardData) == WRONG_EXP_DATE){
+		printf("\nInvalid expireation date.\nPlease, enter a date like this format MM/YY \n");		
+	}
 	
 	printf("Exit Program. \n");
 	
